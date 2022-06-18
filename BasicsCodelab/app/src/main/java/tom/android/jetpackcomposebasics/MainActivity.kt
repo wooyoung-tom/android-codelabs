639bcea6ce4a4d5f5a366f5526020aaf8cb84040
wooyoung <djdj7087@gmail.com>
2022-06-18 16:29:07 +0900

    new codelab

--- BasicsCodelab/app/src/main/java/tom/android/jetpackcomposebasics/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/tom/android/jetpackcomposebasics/MainActivity.kt
@@ -1,29 +1,25 @@
-package tom.android.tiptime
+package tom.android.jetpackcomposebasics
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import tom.android.tiptime.ui.theme.TipTimeTheme
+import tom.android.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipTimeTheme {
+            JetpackComposeBasicsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    TipTimeScreen()
+                    Greeting("Android")
                 }
             }
         }
@@ -31,19 +27,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TipTimeScreen() {
-    Column(
-        modifier = Modifier.padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TipTimeTheme {
-        TipTimeScreen()
+    JetpackComposeBasicsTheme {
+        Greeting("Android")
     }
 }
